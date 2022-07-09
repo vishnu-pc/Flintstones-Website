8188ea425c59275f6d0871ead34fbe3907a1fb5d
vishnu-pc <vishnupc1999@gmail.com>
2022-07-09 18:56:14 +0530

    google-site-verification

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ const Home: NextPage = () => {
             <Head>
                 <title>Flintstones</title>
                 <meta name="description" content="Flintstones official website"/>
+                <meta name="google-site-verification" content="pUMNcxdLDe57HiuLmq5c1gLv3qtmEzkaHn7s8E92z-M" />
                 <link rel="icon" href="images/Logo.svg"/>
             </Head>
 
