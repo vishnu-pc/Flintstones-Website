c9d3fb3e883c5837714499495b4f8866f5e97bba
Thynkk.in <156400276+Thynkkin@users.noreply.github.com>
2024-01-31 17:03:11 +0530

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,11 +8,13 @@ function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
         <Head>
+
         <link rel="icon" href="images/Logo.svg" />
         <link rel="icon" href="images/Logo.svg" sizes="16x16" type="image/png" />
         <link rel="icon" href="images/Logo.svg" sizes="32x32" type="image/png" />
         {/* Add more sizes if needed */}
         <link rel="apple-touch-icon" href="images/Logo.svg" />
+        <link rel="canonical" href="https://www.subikshamcatering.com/"></link>
       </Head>
         <Layout>
             <Component {...pageProps} />
