102db64a77ac1a01aea742c5566adb40bba64108
Thynkk.in <156400276+Thynkkin@users.noreply.github.com>
2024-01-31 13:59:18 +0530

    Add files via upload

--- pages/client_success.tsx
+++ pages/client_success.tsx
@@ -70,8 +70,7 @@ export default function Portfolio() {
           Our results are second to none!
         </h1>
         <p className="font-weight-light font-regular-onMobile">
-        Many of India&apos;s most respected hyper growth startups and unicorn brands trust us to grow their digital businesses.
-
+            Many of Indias most respected hyper growth startups and unicorn brands trust us to grow their digital businesses.
         </p>
       </div>
     </div>
