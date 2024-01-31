268a5bab42103ef6e8a390294b8019e3436c19b3
Thynkk.in <156400276+Thynkkin@users.noreply.github.com>
2024-01-31 16:51:23 +0530

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Layout from '../components/layout';
@@ -6,7 +7,13 @@ import Layout from '../components/layout';
 function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
-        
+        <Head>
+        <link rel="icon" href="images/Logo.svg" />
+        <link rel="icon" href="images/Logo.svg" sizes="16x16" type="image/png" />
+        <link rel="icon" href="images/Logo.svg" sizes="32x32" type="image/png" />
+        {/* Add more sizes if needed */}
+        <link rel="apple-touch-icon" href="images/Logo.svg" />
+      </Head>
         <Layout>
             <Component {...pageProps} />
         </Layout>
